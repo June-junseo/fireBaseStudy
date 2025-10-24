18b148b555ea7892045157e8e4d76c839a3a8a53
June <heojunser@gmail.com>
2025-10-24 13:12:01 +0900

    first commit

---
+++ Assets/Scripts/fireBaseInitializer.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using Firebase;
+using Cysharp.Threading.Tasks;
+
+public class fireBaseInitializer : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+    private void OnDestroy()
+    {
+        
+    }
+}
